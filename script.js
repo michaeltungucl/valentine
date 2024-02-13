b8f83d034fe7debbedd988ef61f15c20913c2647
Michael <tung@hotmail.it>
2024-02-13 19:23:44 +0100

    add text

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ window.addEventListener('DOMContentLoaded', () => {
     const question = document.querySelector('.question');
     const yesBtn = document.querySelector('.yes-btn');
     yesBtn.addEventListener('click', () => {
-        question.innerHTML = 'ti voglio anche io';
+        question.innerHTML = 'HAHAHAHAHAHAHAHA scusami per lo scherzo. Spero ti sia piaciuto. Volevo tanto lasciarti con almeno un sorriso :) ti voglio bene';
     });
 })
 
 const buttonHeight = 50;
 const buttonWidth = 150;
 
-const maxWidth = 1000 - buttonWidth;
-const maxHeight = 600 - buttonHeight;
+const maxWidth = window.innerWidth - buttonWidth;
+const maxHeight = window.innerHeight - buttonHeight;
 
 window.addEventListener('DOMContentLoaded', () => {
     const noButton = document.querySelector('.no-btn');
